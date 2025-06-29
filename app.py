a6ca391ffda199a7463d99ca47aa9387907cecae
THMKruemel <lennartsigge@icloud.com>
2025-06-29 16:46:02 +0200

    add date functionality

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
-from flask import Flask, jsonify
+from datetime import datetime
+from flask import Flask, jsonify, request, redirect
 import scrape
+from scrape import SenneInfo
 
 app = Flask(__name__)
 
 
-@app.route('/json')
+# dateformat: dd-mmm-yy -> 01-Jun-25
+
+
+@app.route('/json', methods=['GET'])
 def is_open_json():
-    message: str = scrape.scrape()
-    senne_open: bool = True
-    if 'Transit Roads Closed' in message:
-        senne_open = False
+    date = request.args.get('date')
+    senne_info: SenneInfo
+    date: str
+    if date == "" or not check_valid_date(date):
+        return redirect(f"/json?date={datetime.today().strftime("%d-%b-%y")}")
+    else:
+        senne_info = scrape.scrape(date)
 
-    return jsonify({"message": message, "isOpen": senne_open})
+    return jsonify({"message": senne_info.message, "isOpen": senne_info.is_open, "date": senne_info.date})
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def is_open():
-    return scrape.scrape()
+    date = request.args.get('date')
+    if date == "" or not check_valid_date(date):
+        return redirect(f"/?date={datetime.today().strftime("%d-%b-%y")}")
+    return scrape.scrape(date).message
+
+
+def check_valid_date(date) -> bool:
+    if not date:
+        return False
+    try:
+        datetime.strptime(date, "%d-%b-%y")
+        return True
+    except ValueError:
+        return False
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- scrape.py
+++ scrape.py
@@ -2,12 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def scrape() -> str:
+def scrape(date):
     response = requests.get('https://bfgnet.de/sennelager-range-access')
+    if response.status_code != 200:
+        return SenneInfo("Could not load information", False, date)
     soup = BeautifulSoup(response.content, 'html.parser')
-    newest_table_row = soup.find_all('tr')[1]
-    open_text: str = ''
-    if newest_table_row:
-        open_text = newest_table_row.text.split('\n')[3]
-    return open_text
+    try:
+        newest_table_row = soup.find(string=date)
+    except IndexError:
+        return SenneInfo("error", False, date)
 
+    open_text: str
+    if newest_table_row:
+        try:
+            open_text = newest_table_row.parent.parent.find_all("td")[2].text
+        except IndexError:
+            return SenneInfo("error", False, date)
+    else:
+        return SenneInfo("error", False, date)
+
+    senne_open: bool = True
+    if 'Transit Roads Closed' in open_text:
+        senne_open = False
+
+    return SenneInfo(open_text, senne_open, date)
+
+
+class SenneInfo:
+    def __init__(self, message: str, is_open: bool, date: str):
+        self.message = message
+        self.is_open = is_open
+        self.date = date
